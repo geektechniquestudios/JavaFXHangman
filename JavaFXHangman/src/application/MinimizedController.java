5e489daa7bcf492202d257f110657e88d765edab
geektechniquestudios <42378648+geektechniquestudios@users.noreply.github.com>
2019-02-16 04:14:03 -0500

    Color scheme updated, options filled

--- JavaFXHangman/src/application/MinimizedController.java
+++ JavaFXHangman/src/application/MinimizedController.java
@@ -28,24 +28,24 @@ import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 public class MinimizedController {
-	
+
 	@FXML
 	private HBox topBox;
 	private Scene optionsScene;
     private Scene mainScene;
-    
+
     @FXML
     private void setSceneOptions()
     {
-    	
+
     }
-    
+
     @FXML
 	private void quitGame()
 	{
 		System.exit(0);
 	}
-    
+
     public void setOptionsScene(Scene someScene)
     {
     	optionsScene = someScene;
@@ -57,18 +57,24 @@ public class MinimizedController {
     }
 
     //@FXML
-    public void openGameScene(ActionEvent actionEvent) 
+    public void openGameScene(ActionEvent actionEvent)
     {
         Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         primaryStage.setScene(mainScene);
         mainScene.getRoot().requestFocus();
     }
-    
+
     public void openOptionsScene(ActionEvent actionEvent) //throws IOException
     {
         Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         primaryStage.setScene(optionsScene);
         optionsScene.getRoot().requestFocus();
+    }
+
+    public void minimizeScene(ActionEvent actionEvent)
+    {
+        Stage primaryStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        primaryStage.setIconified(true);
     }
 
 }
